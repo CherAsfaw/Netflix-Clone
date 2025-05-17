46cf8b2ef6d1809836dca32e9684451f91c58234
Chernet <cherrefera@gmail.com>
2025-05-17 08:06:28 +0300

    axois and request data from the movieDatabase

--- src/component/pages/Home/Home.jsx
+++ src/component/pages/Home/Home.jsx
@@ -5,7 +5,18 @@ import Navbar from '../../Navbar/Navbar'
 const Home = () => {
   return (
     <div className={homeStyle.home}>
-      <Navbar/>
+      <Navbar />
+      <div className={homeStyle.hero}>
+        <img src="" alt="" className={homeStyle.banner_image} />
+        <div className={homeStyle.hero_caption}>
+          <h1></h1>
+          <p></p>
+          <div className={homeStyle.hero_btn}>
+            <button className={homeStyle.common_btn}>Play</button>
+            <button className={`${homeStyle.common_btn} ${homeStyle.dark_btn}`}>More Info</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
