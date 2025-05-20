1fbd0ae546d7a04809e791197533cdf36dd3af3c
Chernet <cherrefera@gmail.com>
2025-05-20 03:15:25 +0300

    some modification

--- src/component/pages/Login/Login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import loginStyle from './login.module.css'
-function Login() {
-  return (
-    <div className={loginStyle.login}>Login</div>
-  )
-}
-
-export default Login
